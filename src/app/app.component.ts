0d6acc09d8782d7742d5b41e9970a1874849c2b0
Leon <leonzorx@gmail.com>
2017-07-09 22:56:47 -0300

    hero details feião funfando na gambiarra

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
-import { Hero } from './hero';
 
+
+  export class Hero {
+    id: number;
+    name: string;
+  }
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -27,10 +31,18 @@ const HEROES: Hero[] = [
     <li *ngFor="let hero of heroes"
     [class.selected]="hero === selectedHero"
     (click)="onSelect(hero)">
-      <span class="badge">{{hero.id}}</span> {{hero.name}}
+      <span class="badge">{{hero.id}}</span>
+       {{hero.name}}
     </li>
   </ul>
-  <hero-detail [hero]="selectedHero"></hero-detail>
+  <div *ngIf="selectedHero">
+    <h2>{{selectedHero.name}} details!</h2>
+    <div><label>id: </label>{{selectedHero.id}}</div>
+    <div><label>name: </label>
+      <input [(ngModel)]="selectedHero.name"
+       placeholder="name"/>
+    </div>
+  </div>
   `,
 
 
@@ -94,5 +106,7 @@ export class AppComponent {
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
   }
+
+
 
 }
